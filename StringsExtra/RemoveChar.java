6cfe5171f70f1bd5b92d324ef8af6772373fc30c
nitinkumar <akfitsz78@gmail.com>
2025-09-18 22:53:58 +0530

    updated

--- StringsExtra/RemoveChar.java
+++
@@ -1,9 +0,0 @@
-public class RemoveChar {
-    public static String removeChar(String str, char ch) {
-        return str.replace(Character.toString(ch), "");
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Modified String: " + removeChar("Hello World", 'l'));
-    }
-}
